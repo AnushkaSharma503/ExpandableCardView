61484a1ad3dc57b77d08d082461018be67a1ae83
AnushkaSharma503 <59316156+AnushkaSharma503@users.noreply.github.com>
2020-10-03 12:48:13 +0530

    Update MainActivity.java
    Update MainActivity.java

--- app/src/main/java/ru/embersoft/expandablecardview/MainActivity.java
+++ app/src/main/java/ru/embersoft/expandablecardview/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // create full screen 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         expandableView = findViewById(R.id.expandableView);
